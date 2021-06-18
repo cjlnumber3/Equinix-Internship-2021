938f6ecc978c55eae696edf54c67ff69a7b2e2c8
Cami <cjlnumber3@outlook.com>
2021-06-18 12:41:56 -0700

    18.6.1241

---
+++ TicTacToeGame (6x6)/src/Tests/MainTest.java
@@ -0,0 +1,414 @@
+package Tests;
+
+import com.MainPackage.Main;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.CoreMatchers.containsString;
+
+class MainTest {
+
+    private TestAccessory mockPlayer;
+
+    @BeforeEach
+    void setUp() {
+        mockPlayer = new TestAccessory();
+    }
+
+    @Test
+    void validateInput() {
+        String playerCoordinates = "3 6";
+        boolean isValid = mockPlayer.validatePlayerCoordinates(playerCoordinates);
+
+        assertTrue(isValid);
+    }
+
+    @Test
+    void playerMove() {
+        int row = 1; //0-5 as per array
+        int column = 1; //0-5 as per array
+        int player = 1; //0, 1 or 3 as per game instructions
+
+        PrintStream original = System.out;
+        ByteArrayOutputStream byteOutputOfPrintPlayerDirections = new ByteArrayOutputStream();
+        PrintStream byteOutputOfPrintPlayerDirectionsToPrintStream = new PrintStream(byteOutputOfPrintPlayerDirections);
+        System.setOut(byteOutputOfPrintPlayerDirectionsToPrintStream);
+
+        Main.setCellStatus(row, column, player);
+        boolean isValid = Main.getValidMovementReturnsTrueIfFree(row, column, player);
+        System.setOut(original);
+        byteOutputOfPrintPlayerDirectionsToPrintStream.flush();
+
+        assertFalse(isValid);
+    }
+
+    @Test
+    void validMovementWhenTaken() {
+        int row = 1; //0-5 as per array
+        int column = 1; //0-5 as per array
+        int player = 1; //0, 1 or 3 as per game instructions
+        String cellStatusTaken = "This move at (" + (row + 1) + "," + (column + 1) + ") is taken.\n";
+
+        PrintStream original = System.out;
+        ByteArrayOutputStream byteOutputOfValidMovement = new ByteArrayOutputStream();
+        PrintStream byteOutputOfValidMovementToPrintStream = new PrintStream(byteOutputOfValidMovement);
+        System.setOut(byteOutputOfValidMovementToPrintStream);
+
+        Main.setCellStatus(row, column, player);
+        Main.validMovement(row, column, player);
+
+        System.setOut(original);
+        String notAValidMovementError = byteOutputOfValidMovement.toString();
+        byteOutputOfValidMovementToPrintStream.flush();
+
+        assertEquals(notAValidMovementError, cellStatusTaken);
+    }
+
+    @Test
+    void validMovementWhenFree() {
+        int player = 1; //0, 1 or 3 as per game instructions
+        int column = 1; //0-5 as per array
+        int row = 1; //0-5 as per array
+
+        Main.setCellStatus(row, column, player);
+        Main.validMovement(row, column, player);
+
+        assertEquals(Main.gameBoard[row][column], player);
+    }
+
+    @Test
+    void printGameBoard() {
+        String emptyGameBoard = "   |   |   |   |   |   \n-----------------------\n   |   |   |   |   |   \n-----------------------\n   |   |   |   |   |   ";
+
+        PrintStream original = System.out;
+        ByteArrayOutputStream byteOutputOfPrintGameBoard = new ByteArrayOutputStream();
+        PrintStream byteOutputOfPrintGameBoardToPrintStream = new PrintStream(byteOutputOfPrintGameBoard);
+        System.setOut(byteOutputOfPrintGameBoardToPrintStream);
+
+        Main.clearGameBoard();
+        Main.printGameBoard();
+        System.setOut(original);
+
+        String actualPrintedGameBoard = byteOutputOfPrintGameBoard.toString();
+        byteOutputOfPrintGameBoardToPrintStream.flush();
+
+        assertThat(actualPrintedGameBoard, containsString(emptyGameBoard));
+    }
+
+    @Test
+    void clearGameBoard() {
+        int row = 1; //0-5 as per array
+        int column = 0; //0-5 as per array
+        int emptyCell = 3;
+
+        Main.clearGameBoard();
+        int clearedCell = Main.getCellStatus(row,  column); //range of 0-2 in array
+
+        assertEquals(emptyCell, clearedCell);
+    }
+
+    @Test
+    void getCellStatus() {
+        int row = 2; //0-5 as per array
+        int column = 2; //0-5 as per array
+        int player = 0; //0, 1, or 3 as per game
+
+        Main.setCellStatus(row, column, player);
+        int cellStatus = Main.getCellStatus(row, column);
+
+        assertEquals(cellStatus, player); //is a free space
+    }
+
+    @Test
+    void printCell() {
+        int row = 1; //0-5 as per array
+        int column = 2; //0-5 as per array
+        int player = 3; //0, 1 or 3 as per game instructions
+
+        Main.setCellStatus(row, column, player);
+        int cellStatus = Main.getCellStatus(row, column);
+        String printedCell = mockPlayer.validatePrintedCell(row, column);
+        boolean isValid = false;
+
+        if (cellStatus == 0 && printedCell.equals(" O ")) {
+            isValid = true;
+        }
+        else if (cellStatus == 1 && printedCell.equals(" X ")) {
+            isValid = true;
+        }
+        else if (cellStatus == 3 && printedCell.equals("   ")) {
+            isValid = true;
+        }
+
+        assertTrue(isValid);
+    }
+
+    @Test
+    public void printWelcomeBanner() {
+        String welcomeBanner = "Welcome to a game of Tic-Tac-Toe! (Player 1 is X, and Player 2 is O)\n";
+
+        PrintStream original = System.out;
+        ByteArrayOutputStream byteOutputOfPrintWelcomeBanner = new ByteArrayOutputStream();
+        PrintStream byteOutputOfPrintWelcomeBannerToPrintStream = new PrintStream(byteOutputOfPrintWelcomeBanner);
+        System.setOut(byteOutputOfPrintWelcomeBannerToPrintStream);
+
+        Main.printWelcomeBanner();
+        System.setOut(original);
+        byteOutputOfPrintWelcomeBannerToPrintStream.flush();
+
+        assertEquals(welcomeBanner, byteOutputOfPrintWelcomeBanner.toString());
+    }
+
+    @Test
+    public void printWinningBanner() {
+        String winnerBannerPlayer1 = "Player 1 (X) has won! Congrats!\n";
+        String winnerBannerPlayer2 = "Player 2 (O) has won! Congrats!\n";
+        String drawBanner = "It's a Draw!\n";
+        int winner = 3; //0, 1 or 3 as per game instructions
+
+        PrintStream original = System.out;
+        ByteArrayOutputStream byteOutputOfPrintWinningBanner = new ByteArrayOutputStream();
+        PrintStream byteOutputOfPrintWinningBannerToPrintStream = new PrintStream(byteOutputOfPrintWinningBanner);
+        System.setOut(byteOutputOfPrintWinningBannerToPrintStream);
+
+        Main.setWinningPlayer(winner);
+        Main.printWinningBanner();
+        System.setOut(original);
+        byteOutputOfPrintWinningBannerToPrintStream.flush();
+
+        assertEquals(drawBanner, byteOutputOfPrintWinningBanner.toString());
+    }
+
+    @Test
+    void printPlayerDirections() {
+        String player1Directions = "Player 1! Please enter the coordinate of your move (row[1-6] column[1-6]): \n";
+        String player2Directions = "Player 2! Please enter the coordinate of your move (row[1-6] column[1-6]): \n";
+        int player = 1; //0, 1 or 3 as per game instructions
+
+        PrintStream original = System.out;
+        ByteArrayOutputStream byteOutputOfPrintPlayerDirections = new ByteArrayOutputStream();
+        PrintStream byteOutputOfPrintPlayerDirectionsToPrintStream = new PrintStream(byteOutputOfPrintPlayerDirections);
+        System.setOut(byteOutputOfPrintPlayerDirectionsToPrintStream);
+
+        Main.printPlayerDirections(player);
+        System.setOut(original);
+        byteOutputOfPrintPlayerDirectionsToPrintStream.flush();
+
+        assertEquals(player1Directions, byteOutputOfPrintPlayerDirections.toString());
+    }
+
+    @Test
+    public void winConditionsDiagonalLeft() {
+        int winningPlayer = 1; // 0, 1 or 3 as per game
+
+        Main.gameBoard[0][0] = winningPlayer;
+        Main.gameBoard[1][1] = winningPlayer;
+        Main.gameBoard[2][2] = winningPlayer;
+        Main.gameBoard[3][3] = winningPlayer;
+        Main.gameBoard[4][4] = winningPlayer;
+        Main.gameBoard[5][5] = winningPlayer;
+        Main.winConditions();
+
+        assertEquals(Main.winningPlayer, winningPlayer);
+    }
+
+    @Test
+    public void winConditionsDiagonalRight() {
+        int winningPlayer = 1; // 0, 1 or 3 as per game
+
+        Main.gameBoard[0][5] = winningPlayer;
+        Main.gameBoard[1][4] = winningPlayer;
+        Main.gameBoard[2][3] = winningPlayer;
+        Main.gameBoard[3][2] = winningPlayer;
+        Main.gameBoard[4][1] = winningPlayer;
+        Main.gameBoard[5][0] = winningPlayer;
+        Main.winConditions();
+
+        assertEquals(Main.winningPlayer, winningPlayer);
+    }
+
+    @Test
+    public void winConditionsHorizontalRow1() {
+        int winningPlayer = 1; // 0, 1 or 3 as per game
+
+        Main.gameBoard[0][0] = winningPlayer;
+        Main.gameBoard[0][1] = winningPlayer;
+        Main.gameBoard[0][2] = winningPlayer;
+        Main.gameBoard[0][3] = winningPlayer;
+        Main.gameBoard[0][4] = winningPlayer;
+        Main.gameBoard[0][5] = winningPlayer;
+        Main.winConditions();
+
+        assertEquals(Main.winningPlayer, winningPlayer);
+    }
+
+    @Test
+    public void winConditionsHorizontalRow2() {
+        int winningPlayer = 1; // 0, 1 or 3 as per game
+
+        Main.gameBoard[1][0] = winningPlayer;
+        Main.gameBoard[1][1] = winningPlayer;
+        Main.gameBoard[1][2] = winningPlayer;
+        Main.gameBoard[1][3] = winningPlayer;
+        Main.gameBoard[1][4] = winningPlayer;
+        Main.gameBoard[1][5] = winningPlayer;
+        Main.winConditions();
+
+        assertEquals(Main.winningPlayer, winningPlayer);
+    }
+
+    @Test
+    public void winConditionsHorizontalRow3() {
+        int winningPlayer = 1; // 0, 1 or 3 as per game
+
+        Main.gameBoard[2][0] = winningPlayer;
+        Main.gameBoard[2][1] = winningPlayer;
+        Main.gameBoard[2][2] = winningPlayer;
+        Main.gameBoard[2][3] = winningPlayer;
+        Main.gameBoard[2][4] = winningPlayer;
+        Main.gameBoard[2][5] = winningPlayer;
+        Main.winConditions();
+
+        assertEquals(Main.winningPlayer, winningPlayer);
+    }
+
+    @Test
+    public void winConditionsHorizontalRow4() {
+        int winningPlayer = 1; // 0, 1 or 3 as per game
+
+        Main.gameBoard[3][0] = winningPlayer;
+        Main.gameBoard[3][1] = winningPlayer;
+        Main.gameBoard[3][2] = winningPlayer;
+        Main.gameBoard[3][3] = winningPlayer;
+        Main.gameBoard[3][4] = winningPlayer;
+        Main.gameBoard[3][5] = winningPlayer;
+        Main.winConditions();
+
+        assertEquals(Main.winningPlayer, winningPlayer);
+    }
+
+    @Test
+    public void winConditionsHorizontalRow5() {
+        int winningPlayer = 1; // 0, 1 or 3 as per game
+
+        Main.gameBoard[4][0] = winningPlayer;
+        Main.gameBoard[4][1] = winningPlayer;
+        Main.gameBoard[4][2] = winningPlayer;
+        Main.gameBoard[4][3] = winningPlayer;
+        Main.gameBoard[4][4] = winningPlayer;
+        Main.gameBoard[4][5] = winningPlayer;
+        Main.winConditions();
+
+        assertEquals(Main.winningPlayer, winningPlayer);
+    }
+
+    @Test
+    public void winConditionsHorizontalRow6() {
+        int winningPlayer = 1; // 0, 1 or 3 as per game
+
+        Main.gameBoard[5][0] = winningPlayer;
+        Main.gameBoard[5][1] = winningPlayer;
+        Main.gameBoard[5][2] = winningPlayer;
+        Main.gameBoard[5][3] = winningPlayer;
+        Main.gameBoard[5][4] = winningPlayer;
+        Main.gameBoard[5][5] = winningPlayer;
+        Main.winConditions();
+
+        assertEquals(Main.winningPlayer, winningPlayer);
+    }
+
+    @Test
+    public void winConditionsVerticalColumn1() {
+        int winningPlayer = 1; // 0, 1 or 3 as per game
+
+        Main.gameBoard[0][0] = winningPlayer;
+        Main.gameBoard[1][0] = winningPlayer;
+        Main.gameBoard[2][0] = winningPlayer;
+        Main.gameBoard[3][0] = winningPlayer;
+        Main.gameBoard[4][0] = winningPlayer;
+        Main.gameBoard[5][0] = winningPlayer;
+        Main.winConditions();
+
+        assertEquals(Main.winningPlayer, winningPlayer);
+    }
+
+    @Test
+    public void winConditionsVerticalColumn2() {
+        int winningPlayer = 1; // 0, 1 or 3 as per game
+
+        Main.gameBoard[0][1] = winningPlayer;
+        Main.gameBoard[1][1] = winningPlayer;
+        Main.gameBoard[2][1] = winningPlayer;
+        Main.gameBoard[3][1] = winningPlayer;
+        Main.gameBoard[4][1] = winningPlayer;
+        Main.gameBoard[5][1] = winningPlayer;
+        Main.winConditions();
+
+        assertEquals(Main.winningPlayer, winningPlayer);
+    }
+
+    @Test
+    public void winConditionsVerticalColumn3() {
+        int winningPlayer = 1; // 0, 1 or 3 as per game
+
+        Main.gameBoard[0][2] = winningPlayer;
+        Main.gameBoard[1][2] = winningPlayer;
+        Main.gameBoard[2][2] = winningPlayer;
+        Main.gameBoard[3][2] = winningPlayer;
+        Main.gameBoard[4][2] = winningPlayer;
+        Main.gameBoard[5][2] = winningPlayer;
+        Main.winConditions();
+
+        assertEquals(Main.winningPlayer, winningPlayer);
+    }
+
+    @Test
+    public void winConditionsVerticalColumn4() {
+        int winningPlayer = 1; // 0, 1 or 3 as per game
+
+        Main.gameBoard[0][3] = winningPlayer;
+        Main.gameBoard[1][3] = winningPlayer;
+        Main.gameBoard[2][3] = winningPlayer;
+        Main.gameBoard[3][3] = winningPlayer;
+        Main.gameBoard[4][3] = winningPlayer;
+        Main.gameBoard[5][3] = winningPlayer;
+        Main.winConditions();
+
+        assertEquals(Main.winningPlayer, winningPlayer);
+    }
+
+    @Test
+    public void winConditionsVerticalColumn5() {
+        int winningPlayer = 1; // 0, 1 or 3 as per game
+
+        Main.gameBoard[0][4] = winningPlayer;
+        Main.gameBoard[1][4] = winningPlayer;
+        Main.gameBoard[2][4] = winningPlayer;
+        Main.gameBoard[3][4] = winningPlayer;
+        Main.gameBoard[4][4] = winningPlayer;
+        Main.gameBoard[5][4] = winningPlayer;
+        Main.winConditions();
+
+        assertEquals(Main.winningPlayer, winningPlayer);
+    }
+    @Test
+    public void winConditionsVerticalColumn6() {
+        int winningPlayer = 1; // 0, 1 or 3 as per game
+
+        Main.gameBoard[0][5] = winningPlayer;
+        Main.gameBoard[1][5] = winningPlayer;
+        Main.gameBoard[2][5] = winningPlayer;
+        Main.gameBoard[3][5] = winningPlayer;
+        Main.gameBoard[4][5] = winningPlayer;
+        Main.gameBoard[5][5] = winningPlayer;
+        Main.winConditions();
+
+        assertEquals(Main.winningPlayer, winningPlayer);
+    }
+
+}
---
+++ TicTacToeGame (6x6)/src/com/MainPackage/Main.java
@@ -0,0 +1,266 @@
+package com.MainPackage;
+
+
+import java.util.Scanner;
+
+public class Main {
+    public static Scanner playerInput = new Scanner(System.in);
+
+    private static int playerCount = 1;
+
+
+    public static int[][] gameBoard = new int[6][6];
+
+
+    public static int winningPlayer;
+
+
+
+    public static void main(String[] args) {
+
+        clearGameBoard();
+
+        printWelcomeBanner();
+
+        runGame();
+
+        printGameBoard();
+
+        printWinningBanner();
+    }
+
+    public static void runGame() {
+
+        initializeGame();
+
+        playerMove(playerCount);
+
+        do {
+            playerCount++;
+            printGameBoard();
+            playerMove(playerCount % 2);
+        } while (!winConditions());
+    }
+
+    public static void playerMove(int currentPlayer) {
+
+        boolean validInput = false;
+
+        do {
+            printPlayerDirections(currentPlayer);
+            int row = playerInput.nextInt() - 1;
+            int column = playerInput.nextInt() - 1;
+
+            validInput = validMovement(row, column, currentPlayer);
+        } while (!validInput);
+    }
+
+    public static boolean getValidMovementReturnsTrueIfFree(int row, int column, int player) {
+        int emptyCell = 3;
+
+        if (gameBoard[row][column] != emptyCell) {
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean validMovement(int row, int column, int currentPlayer) {
+        int numberOfGameBoardRows = 6;
+        int numberOfGameBoardColumns = 6;
+
+        int emptyCell = 3;
+
+        boolean validInput = false;
+
+        if (row >= 0 && row < numberOfGameBoardRows && column >= 0 && column < numberOfGameBoardColumns && gameBoard[row][column] == emptyCell) {
+            gameBoard[row][column] = currentPlayer;
+            return true;
+        }
+        else {
+            System.out.print("This move at (" + (row + 1) + "," + (column + 1) + ") is taken.\n");
+        }
+
+        return false;
+    }
+
+    public static void printGameBoard() {
+        int numberOfGameBoardRows = 6;
+        int numberOfGameBoardColumns = 6;
+
+        for (int row = 0; row < numberOfGameBoardRows; ++row) {
+            for (int column = 0; column < numberOfGameBoardColumns; ++column) {
+                printCell(gameBoard[row][column]);
+                if (column != numberOfGameBoardColumns - 1) {
+                    System.out.print("|");
+                }
+            }
+            System.out.println();
+            if (row != numberOfGameBoardRows - 1) {
+                System.out.println("-----------------------");
+            }
+        }
+        System.out.println();
+    }
+
+    public static void clearGameBoard() {
+        int numberOfGameBoardRows = 6;
+        int numberOfGameBoardColumns = 6;
+
+        for (int row = 0; row < numberOfGameBoardRows; ++row) {
+            for (int col = 0; col < numberOfGameBoardColumns; ++col) {
+                gameBoard[row][col] = 3;
+            }
+        }
+    }
+
+    public static int getCellStatus(int row, int column) {
+        return gameBoard[row][column];
+    }
+
+    public static void setCellStatus(int row, int column, int player) {
+        gameBoard[row][column] = player;
+    }
+
+    public static void printCell(int player) {
+        final int emptyCell = 3;
+        final int XCell = 1;
+        final int OCell = 0;
+
+        switch (player) {
+            case emptyCell: {
+                System.out.print("   ");
+            } break; //0
+            case OCell: {
+                System.out.print(" O ");
+            } break; //2
+            case XCell:  {
+                System.out.print(" X ");
+            } break; //1
+        }
+    }
+
+
+
+    public static boolean winConditions() {
+
+        //horizontal rows are true for X/1
+        if (gameBoard[0][0] == 1 && gameBoard[0][1] == 1 && gameBoard[0][2] == 1 && gameBoard[0][3] == 1 && gameBoard[0][4] == 1 && gameBoard[0][5] == 1
+                || gameBoard[1][0] == 1 && gameBoard[1][1] == 1 && gameBoard[1][2] == 1 && gameBoard[1][3] == 1 && gameBoard[1][4] == 1 && gameBoard[1][5] == 1
+                || gameBoard[2][0] == 1 && gameBoard[2][1] == 1 && gameBoard[2][2] == 1 && gameBoard[2][3] == 1 && gameBoard[2][4] == 1 && gameBoard[2][5] == 1
+                || gameBoard[3][0] == 1 && gameBoard[3][1] == 1 && gameBoard[3][2] == 1 && gameBoard[3][3] == 1 && gameBoard[3][4] == 1 && gameBoard[3][5] == 1
+                || gameBoard[4][0] == 1 && gameBoard[4][1] == 1 && gameBoard[4][2] == 1 && gameBoard[4][3] == 1 && gameBoard[4][4] == 1 && gameBoard[4][5] == 1
+                || gameBoard[5][0] == 1 && gameBoard[5][1] == 1 && gameBoard[5][2] == 1 && gameBoard[5][3] == 1 && gameBoard[5][4] == 1 && gameBoard[5][5] == 1) {
+
+            setWinningPlayer(1);
+            return true;
+        }
+        //vertical columns are true for X/1
+        else if (gameBoard[0][0] == 1 && gameBoard[1][0] == 1 && gameBoard[2][0] == 1 && gameBoard[3][0] == 1 && gameBoard[4][0] == 1 && gameBoard[5][0] == 1
+                || gameBoard[0][1] == 1 && gameBoard[1][1] == 1 && gameBoard[2][1] == 1 && gameBoard[3][1] == 1 && gameBoard[4][1] == 1 && gameBoard[5][1] == 1
+                || gameBoard[0][2] == 1 && gameBoard[1][2] == 1 && gameBoard[2][2] == 1 && gameBoard[3][2] == 1 && gameBoard[4][2] == 1 && gameBoard[5][2] == 1
+                || gameBoard[0][3] == 1 && gameBoard[1][3] == 1 && gameBoard[2][3] == 1 && gameBoard[3][3] == 1 && gameBoard[4][3] == 1 && gameBoard[5][3] == 1
+                || gameBoard[0][4] == 1 && gameBoard[1][4] == 1 && gameBoard[2][4] == 1 && gameBoard[3][4] == 1 && gameBoard[4][4] == 1 && gameBoard[5][4] == 1
+                || gameBoard[0][5] == 1 && gameBoard[1][5] == 1 && gameBoard[2][5] == 1 && gameBoard[3][5] == 1 && gameBoard[4][5] == 1 && gameBoard[5][5] == 1) {
+
+            setWinningPlayer(1);
+            return true;
+        }
+        //diagonal are true for X/1
+        else if (gameBoard[0][0] == 1 && gameBoard[1][1] == 1 && gameBoard[2][2] == 1 && gameBoard[3][3] == 1 && gameBoard[4][4] == 1 && gameBoard[5][5] == 1
+                || gameBoard[0][5] == 1 && gameBoard[1][4] == 1 && gameBoard[2][3] == 1 && gameBoard[3][2] == 1 && gameBoard[4][1] == 1 && gameBoard[5][0] == 1) {
+
+            setWinningPlayer(1);
+            return true;
+        }
+
+        //horizontal rows are true for O/2
+        else if (gameBoard[0][0] == 0 && gameBoard[0][1] == 0 && gameBoard[0][2] == 0 && gameBoard[0][3] == 0 && gameBoard[0][4] == 0 && gameBoard[0][5] == 0
+                || gameBoard[1][0] == 0 && gameBoard[1][1] == 0 && gameBoard[1][2] == 0 && gameBoard[1][3] == 0 && gameBoard[1][4] == 0 && gameBoard[1][5] == 0
+                || gameBoard[2][0] == 0 && gameBoard[2][1] == 0 && gameBoard[2][2] == 0 && gameBoard[2][3] == 0 && gameBoard[2][4] == 0 && gameBoard[2][5] == 0
+                || gameBoard[3][0] == 0 && gameBoard[3][1] == 0 && gameBoard[3][2] == 0 && gameBoard[3][3] == 0 && gameBoard[3][4] == 0 && gameBoard[3][5] == 0
+                || gameBoard[4][0] == 0 && gameBoard[4][1] == 0 && gameBoard[4][2] == 0 && gameBoard[4][3] == 0 && gameBoard[4][4] == 0 && gameBoard[4][5] == 0
+                || gameBoard[5][0] == 0 && gameBoard[5][1] == 0 && gameBoard[5][2] == 0 && gameBoard[5][3] == 0 && gameBoard[5][4] == 0 && gameBoard[5][5] == 0) {
+
+            setWinningPlayer(0);
+            return true;
+        }
+        //vertical columns are true for O/2
+        else if (gameBoard[0][0] == 0 && gameBoard[1][0] == 0 && gameBoard[2][0] == 0 && gameBoard[3][0] == 0 && gameBoard[4][0] == 0 && gameBoard[5][0] == 0
+                || gameBoard[0][1] == 0 && gameBoard[1][1] == 0 && gameBoard[2][1] == 0 && gameBoard[3][1] == 0 && gameBoard[4][1] == 0 && gameBoard[5][1] == 0
+                || gameBoard[0][2] == 0 && gameBoard[1][2] == 0 && gameBoard[2][2] == 0 && gameBoard[3][2] == 0 && gameBoard[4][2] == 0 && gameBoard[5][2] == 0
+                || gameBoard[0][3] == 0 && gameBoard[1][3] == 0 && gameBoard[2][3] == 0 && gameBoard[3][3] == 0 && gameBoard[4][3] == 0 && gameBoard[5][3] == 0
+                || gameBoard[0][4] == 0 && gameBoard[1][4] == 0 && gameBoard[2][4] == 0 && gameBoard[3][4] == 0 && gameBoard[4][4] == 0 && gameBoard[5][4] == 0
+                || gameBoard[0][5] == 0 && gameBoard[1][5] == 0 && gameBoard[2][5] == 0 && gameBoard[3][5] == 0 && gameBoard[4][5] == 0 && gameBoard[5][5] == 0) {
+
+            setWinningPlayer(0);
+            return true;
+        }
+        //diagonal are true for O/2
+        else if (gameBoard[0][0] == 0 && gameBoard[1][1] == 0 && gameBoard[2][2] == 0 && gameBoard[3][3] == 0 && gameBoard[4][4] == 0 && gameBoard[5][5] == 0
+                || gameBoard[0][5] == 0 && gameBoard[1][4] == 0 && gameBoard[2][3] == 0 && gameBoard[3][2] == 0 && gameBoard[4][1] == 0 && gameBoard[5][0] == 0) {
+
+            setWinningPlayer(0);
+            return true;
+        }
+        else if (gameBoard[0][0] != 3 && gameBoard[0][1] != 3 && gameBoard[0][2] != 3 && gameBoard[0][3] != 3 && gameBoard[0][4] != 3 && gameBoard[0][5] != 3
+                && gameBoard[1][0] != 3 && gameBoard[1][1] != 3 && gameBoard[1][2] != 3 && gameBoard[1][3] != 3 && gameBoard[1][4] != 3 && gameBoard[1][5] != 3
+                && gameBoard[2][0] != 3 && gameBoard[2][1] != 3 && gameBoard[2][2] != 3 && gameBoard[2][3] != 3 && gameBoard[2][4] != 3 && gameBoard[2][5] != 3
+                && gameBoard[3][0] != 3 && gameBoard[3][1] != 3 && gameBoard[3][2] != 3 && gameBoard[3][3] != 3 && gameBoard[3][4] != 3 && gameBoard[3][5] != 3
+                && gameBoard[4][0] != 3 && gameBoard[4][1] != 3 && gameBoard[4][2] != 3 && gameBoard[4][3] != 3 && gameBoard[4][4] != 3 && gameBoard[4][5] != 3
+                && gameBoard[5][0] != 3 && gameBoard[5][1] != 3 && gameBoard[5][2] != 3 && gameBoard[5][3] != 3 && gameBoard[5][4] != 3 && gameBoard[5][5] != 3
+                && gameBoard[0][0] != 3 && gameBoard[1][0] != 3 && gameBoard[2][0] != 3 && gameBoard[3][0] != 3 && gameBoard[4][0] != 3 && gameBoard[5][0] != 3
+                && gameBoard[0][1] != 3 && gameBoard[1][1] != 3 && gameBoard[2][1] != 3 && gameBoard[3][1] != 3 && gameBoard[4][1] != 3 && gameBoard[5][1] != 3
+                && gameBoard[0][2] != 3 && gameBoard[1][2] != 3 && gameBoard[2][2] != 3 && gameBoard[3][2] != 3 && gameBoard[4][2] != 3 && gameBoard[5][2] != 3
+                && gameBoard[0][3] != 3 && gameBoard[1][3] != 3 && gameBoard[2][3] != 3 && gameBoard[3][3] != 3 && gameBoard[4][3] != 3 && gameBoard[5][3] != 3
+                && gameBoard[0][4] != 3 && gameBoard[1][4] != 3 && gameBoard[2][4] != 3 && gameBoard[3][4] != 3 && gameBoard[4][4] != 3 && gameBoard[5][4] != 3
+                && gameBoard[0][5] != 3 && gameBoard[1][5] != 3 && gameBoard[2][5] != 3 && gameBoard[3][5] != 3 && gameBoard[4][5] != 3 && gameBoard[5][5] != 3) {
+
+            setWinningPlayer(3);
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public static void printWelcomeBanner() {
+        System.out.print("Welcome to a game of Tic-Tac-Toe! (Player 1 is X, and Player 2 is O)\n");
+    }
+
+    public static void printWinningBanner() {
+        if (winningPlayer == 1) {
+            System.out.print("Player 1 (X) has won! Congrats!\n");
+        } else if (winningPlayer == 0) {
+            System.out.print("Player 2 (O) has won! Congrats!\n");
+        } else if (winningPlayer == 3) {
+            System.out.print("It's a Draw!\n");
+        }
+    }
+
+    public static void setWinningPlayer(int player) {
+        winningPlayer = player;
+    }
+
+    public static void initializeGame() {
+        System.out.println("Press \"ENTER\" to continue...");
+        Scanner scanner = new Scanner(System.in);
+        scanner.nextLine();
+    }
+
+    public static void printPlayerDirections(int currentPlayer) {
+        int XCell = 1;
+        int OCell = 0;
+
+        String playerDirectionsString;
+
+        if (currentPlayer == XCell) {
+            playerDirectionsString = "Player 1! Please enter the coordinate of your move (row[1-6] column[1-6]): \n";
+            System.out.print(playerDirectionsString);
+        }
+        else if (currentPlayer == OCell) {
+            playerDirectionsString = "Player 2! Please enter the coordinate of your move (row[1-6] column[1-6]): \n";
+            System.out.println(playerDirectionsString);
+        }
+    }
+
+}
